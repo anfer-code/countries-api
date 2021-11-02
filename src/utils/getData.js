ac8c19d24933aadc244d16e900781a93b8c90c6d
Anfernee Valera <81828628+anfer-code@users.noreply.github.com>
2021-11-02 11:27:21 -0400

    Update getData.js

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,8 @@
-const API = "https://restcountries.eu/rest/v2/all/"
+const API = "https://restcountries.eu/rest/v2/all/?access_key=bd1274754f57fb2c514ed78cba480de4
+"
 
 const getData = async (id) => {
-    const apiUrl = id ? `${API.replace("all/", id)}` : API
+    const apiUrl = id ? `${API.replace("all", id)}` : API
 
     try {
         const response = await fetch(apiUrl)
